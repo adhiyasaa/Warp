820daf776e812a68c0ec042e2f19eb49e34c4dd4
Muhammad Varel Antoni <126854041+VarelAntoni@users.noreply.github.com>
2025-09-13 01:57:17 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8000;
 
 app.use(
   cors({
-    origin: "https://warp.up.railway.app",
+    origin: "https://warp1.up.railway.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
